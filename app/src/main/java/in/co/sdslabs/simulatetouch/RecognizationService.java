235348ce72c8de8dc497f5484e670829d4cb1677
Arpit Singla <arpit270896@gmail.com>
2015-10-18 06:19:31 +0530

    changed icon and added floating head

--- app/src/main/java/in/co/sdslabs/simulatetouch/RecognizationService.java
+++ app/src/main/java/in/co/sdslabs/simulatetouch/RecognizationService.java
@@ -2,12 +2,21 @@ package in.co.sdslabs.simulatetouch;
 
 import android.app.Application;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import java.io.DataOutputStream;
 import java.io.File;
@@ -36,20 +45,72 @@ public class RecognizationService extends Service implements RecognitionListener
     private static final List<String> WORDS_LIST = Arrays.asList("up", "down", "left", "right");
     private static final String KEYPHRASE = "ok google";
 
+
     private SpeechRecognizer recognizer;
     private Process process;
     private DataOutputStream os;
     private String cmd;
+    private WindowManager windowManager;
+    private ImageView floatingHead;
+    private WindowManager.LayoutParams layoutParams;
+    private Display display;
+    private Point screenSize = new Point(0,0);
 
     @Override
     public void onCreate() {
         Log.d("In service: ", "service started");
+
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        display = windowManager.getDefaultDisplay();
+        display.getSize(screenSize);
+
+        floatingHead = new ImageView(this);
+        floatingHead.setBackgroundResource(R.mipmap.floating_head);
+
+        layoutParams = new WindowManager.LayoutParams(
+                (int)Math.sqrt(screenSize.x*screenSize.y/100*Math.PI), (int)Math.sqrt(screenSize.x*screenSize.y/100*Math.PI),
+                WindowManager.LayoutParams.TYPE_PHONE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        layoutParams.gravity = Gravity.TOP | Gravity.START;
+        layoutParams.x = 0;
+        layoutParams.y = 100;
 
         try {
             process = Runtime.getRuntime().exec("su");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        floatingHead.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = layoutParams.x;
+                        initialY = layoutParams.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        layoutParams.x = initialX
+                                + (int) (event.getRawX() - initialTouchX);
+                        layoutParams.y = initialY
+                                + (int) (event.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(floatingHead, layoutParams);
+                        return true;
+                }
+                return false;
+            }
+        });
 
         os = new DataOutputStream(process.getOutputStream());
 
@@ -75,10 +136,10 @@ public class RecognizationService extends Service implements RecognitionListener
                 else {
                     recognizer.stop();
                     recognizer.startListening(KWS_SEARCH);
+                    windowManager.addView(floatingHead, layoutParams);
                 }
             }
         }.execute();
-
 
     }
 
